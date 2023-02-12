a1e67463b99be52cad279d8fbdd3807f486d02bb
vaibhavk <you@example.com>
2023-02-13 00:52:22 +0530

    java project first commit

---
+++ src/test/java/com/example/ReactSpring/ReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReactSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
